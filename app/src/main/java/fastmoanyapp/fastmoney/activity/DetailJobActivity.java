5b5f3f5e346eed2fe019234608cad1bfa6d40cae
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2017-11-26 15:32:25 -0500

    Adding job detail

---
+++ app/src/main/java/fastmoanyapp/fastmoney/activity/DetailJobActivity.java
@@ -0,0 +1,15 @@
+package fastmoanyapp.fastmoney.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import fastmoanyapp.fastmoney.R;
+
+public class DetailJobActivity extends FragmentActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_job);
+
+    }
+}
