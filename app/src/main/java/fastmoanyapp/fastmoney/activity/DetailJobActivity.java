0d6fbb18c43d2b41d64d8e2a33ea7730ab4163a2
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2017-11-27 16:48:02 -0500

    Detail page

--- app/src/main/java/fastmoanyapp/fastmoney/activity/DetailJobActivity.java
+++ app/src/main/java/fastmoanyapp/fastmoney/activity/DetailJobActivity.java
@@ -2,14 +2,43 @@ package fastmoanyapp.fastmoney.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import fastmoanyapp.fastmoney.R;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class DetailJobActivity extends FragmentActivity{
+import java.util.ArrayList;
+import java.util.List;
+
+import fastmoanyapp.fastmoney.R;
+import ss.com.bannerslider.banners.Banner;
+import ss.com.bannerslider.banners.RemoteBanner;
+import ss.com.bannerslider.events.OnBannerClickListener;
+import ss.com.bannerslider.views.BannerSlider;
+
+public class DetailJobActivity extends AppCompatActivity{
+
+    BannerSlider detail_job_images;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_job);
+        getSupportActionBar().hide();
+
+        BannerSlider bannerSlider = (BannerSlider) findViewById(R.id.detail_job_images);
+        List<Banner> banners=new ArrayList<>();
+        //add banner using image url
+        banners.add(new RemoteBanner("https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2015/07/freelancer.jpg"));
+        banners.add(new RemoteBanner("https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2015/07/freelancer.jpg"));
+        banners.add(new RemoteBanner("https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2015/07/freelancer.jpg"));
+        //add banner using resource drawable
+        bannerSlider.setBanners(banners);
+
+        bannerSlider.setOnBannerClickListener(new OnBannerClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(DetailJobActivity.this, "Banner with position " + String.valueOf(position) + " clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
--- app/src/main/java/fastmoanyapp/fastmoney/utils/utils.java
+++ app/src/main/java/fastmoanyapp/fastmoney/utils/utils.java
@@ -1,6 +1,19 @@
 package fastmoanyapp.fastmoney.utils;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by Usuario on 30/05/2017.
@@ -17,5 +30,20 @@ public class utils {
 
     public static boolean isEmpty(EditText myeditText) {
         return myeditText.getText().toString().trim().length() == 0;
+    }
+
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            // Log exception
+            return null;
+        }
     }
 }
